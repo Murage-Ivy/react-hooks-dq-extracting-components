1c48702ce0585b3e9b9525ed722ae4ef7784f655
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-09-23 10:23:37 +0300

    Separate componnents

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav ({contacts}) {
+    const contactList = contacts.map((contact) => <li key={contact.id} className="contact">
+        <div className='icon'>{contact.name.charAt(0)}</div>
+        {contact.name}
+    </li>)
+    return (
+        <nav>
+        <h2>Contacts</h2>
+        <ul className="contacts">
+            {contactList}
+        </ul>
+      </nav>
+    )
+}
+
+export default Nav;
